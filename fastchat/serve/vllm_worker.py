8b691f4aa516220b9f6e2fd7866ccff611646c7f
gaoyuan-zhang <gaoyuan.zhang@ibm.com>
2024-03-08 05:37:34 +0000

    missing repetition penalty in vllm

--- fastchat/serve/vllm_worker.py
+++ fastchat/serve/vllm_worker.py
@@ -74,6 +74,7 @@ class VLLMWorker(BaseModelWorker):
         top_k = params.get("top_k", -1.0)
         presence_penalty = float(params.get("presence_penalty", 0.0))
         frequency_penalty = float(params.get("frequency_penalty", 0.0))
+        repetition_penalty = float(params.get("repetition_penalty", 0.0))
         max_new_tokens = params.get("max_new_tokens", 256)
         stop_str = params.get("stop", None)
         stop_token_ids = params.get("stop_token_ids", None) or []
@@ -114,6 +115,7 @@ class VLLMWorker(BaseModelWorker):
             top_k=top_k,
             presence_penalty=presence_penalty,
             frequency_penalty=frequency_penalty,
+            repetition_penalty=repetition_penalty,
             best_of=best_of,
         )
         results_generator = engine.generate(context, sampling_params, request_id)
@@ -156,9 +158,11 @@ class VLLMWorker(BaseModelWorker):
                 "cumulative_logprob": [
                     output.cumulative_logprob for output in request_output.outputs
                 ],
-                "finish_reason": request_output.outputs[0].finish_reason
-                if len(request_output.outputs) == 1
-                else [output.finish_reason for output in request_output.outputs],
+                "finish_reason": (
+                    request_output.outputs[0].finish_reason
+                    if len(request_output.outputs) == 1
+                    else [output.finish_reason for output in request_output.outputs]
+                ),
             }
             # Emit twice here to ensure a 'finish_reason' with empty content in the OpenAI API response.
             # This aligns with the behavior of model_worker.
