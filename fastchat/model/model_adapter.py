57c7712a19d6193b490ba3cf68ee6d46530c32d7
Veronique Demers <vdemers@ibm.com>
2024-08-06 17:26:17 +0000

    dolomite

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -87,14 +87,62 @@ OPENAI_MODEL_LIST = (
     "gpt-4o-2024-05-13",
 )
 
-from fastchat.model.base_model_adapter import BaseModelAdapter
-from fastchat.model.custom_adapters import (
-    IbmLabradoriteAdapter, 
-    IbmModelBigCodeAdapter, 
-    IbmModelMegatronAdapter,
-    IbmModelDolomiteAdapter
-)
+# from fastchat.model.base_model_adapter import BaseModelAdapter
+# from fastchat.model.custom_adapters import (
+#     IbmLabradoriteAdapter, 
+#     IbmModelBigCodeAdapter, 
+#     IbmModelMegatronAdapter,
+#     IbmModelDolomiteAdapter
+# )
 
+class BaseModelAdapter:
+    """The base and the default model adapter."""
+
+    use_fast_tokenizer = True
+
+    def match(self, model_path: str):
+        return True
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        revision = from_pretrained_kwargs.get("revision", "main")
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(
+                model_path,
+                use_fast=self.use_fast_tokenizer,
+                revision=revision,
+                trust_remote_code=True,
+            )
+        except TypeError:
+            tokenizer = AutoTokenizer.from_pretrained(
+                model_path, use_fast=False, revision=revision, trust_remote_code=True
+            )
+        try:
+            model = AutoModelForCausalLM.from_pretrained(
+                model_path,
+                low_cpu_mem_usage=True,
+                trust_remote_code=True,
+                **from_pretrained_kwargs,
+            )
+        except NameError:
+            model = AutoModel.from_pretrained(
+                model_path,
+                low_cpu_mem_usage=True,
+                trust_remote_code=True,
+                **from_pretrained_kwargs,
+            )
+        return model, tokenizer
+
+    def load_compress_model(self, model_path, device, torch_dtype, revision="main"):
+        return load_compress_model(
+            model_path,
+            device,
+            torch_dtype,
+            use_fast=self.use_fast_tokenizer,
+            revision=revision,
+        )
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("one_shot")
 
 # A global registry for all model adapters
 # TODO (lmzheng): make it a priority queue.
@@ -2402,6 +2450,137 @@ class RekaAdapter(BaseModelAdapter):
         return get_conv_template("api_based_default")
 
 
+class IbmLabradoriteAdapter(BaseModelAdapter):
+    """The base and the default model adapter."""
+
+    # use_fast_tokenizer = True
+
+    def match(self, model_path: str):
+        print(f"IBM Labradorite adapter: {'labradorite' in model_path.lower()}")
+        return "labradorite" in model_path.lower()
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("labradorite")
+
+
+class IbmModelBigCodeAdapter(BaseModelAdapter):
+    """The base and the default model adapter."""
+    print("Trying IBM BigCode adapter")
+
+    use_fast_tokenizer = True
+
+    def match(self, model_path: str):
+        return "ibm" in model_path.lower() and "bigcode" in model_path.lower()
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        revision = from_pretrained_kwargs.get("revision", "main")
+        print("Loading from IBM BigCode adapter")
+        try:
+            # Changed from torch.bloat16 to torch.float16
+            model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype=torch.float16)
+            tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="left")
+            model.eval()
+        except Exception as e:
+            raise ValueError(f"AutoModelForCausalLM is not found or unable to load the model: {e}")
+        else:
+            return model, tokenizer
+
+    def load_compress_model(self, model_path, device, torch_dtype, revision="main"):
+        return load_compress_model(
+            model_path,
+            device,
+            torch_dtype,
+            use_fast=self.use_fast_tokenizer,
+            revision=revision,
+        )
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("ibm-labrador")
+
+
+class IbmModelMegatronAdapter(BaseModelAdapter):
+    """The base and the default model adapter."""
+    print("Trying IBM Megatron adapter")
+
+    use_fast_tokenizer = True
+
+    def match(self, model_path: str):
+        return "ibm" in model_path.lower() and "megatron" in model_path.lower()
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        try:
+            from ibm_models import GPTMegatronForCausalLM
+        except:
+            raise Exception("ibm-models package not installed, please install separately.")
+        
+        revision = from_pretrained_kwargs.get("revision", "main")
+        print("Loading from IBM Megatron adapter")
+        try:
+            model = GPTMegatronForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype=torch.bfloat16)
+            tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="left")
+            model.eval()
+        except Exception as e:
+            raise ValueError(f"GPTMegatronForCausalLM is not found or unable to load the model: {e}")
+        else:
+            return model, tokenizer
+
+    def load_compress_model(self, model_path, device, torch_dtype, revision="main"):
+        return load_compress_model(
+            model_path,
+            device,
+            torch_dtype,
+            use_fast=self.use_fast_tokenizer,
+            revision=revision,
+        )
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("ibm-labrador")
+
+
+class IbmModelDolomiteAdapter(BaseModelAdapter):
+    """The base and the default model adapter."""
+    print("Trying IBM Dolomite adapter")
+
+    use_fast_tokenizer = True
+
+    def match(self, model_path: str):
+        print(f"IBM Dolomite adapter: {'dolomite' in model_path.lower()}")
+        return "ibm" in model_path.lower() and "dolomite" in model_path.lower()
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        try:
+            from dolomite_engine.hf_models import GPTDolomiteForCausalLM
+        except:
+            raise Exception("dolomite_engine package not installed, please install separately.")
+        
+        revision = from_pretrained_kwargs.get("revision", "main")
+        print("Loading from IBM Megatron adapter")
+        try:
+            model = GPTDolomiteForCausalLM.from_pretrained(
+                model_path,
+                attn_implementation="flash_attention_2",
+                use_padding_free_transformer=False,
+                trust_remote_code=True, # Might need to be removed
+                torch_dtype=torch.bfloat16 # Might need to be removed
+            )
+            tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="left")
+            model.eval()
+        except Exception as e:
+            raise ValueError(f"GPTDolomiteForCausalLM is not found or unable to load the model: {e}")
+        else:
+            return model, tokenizer
+
+    def load_compress_model(self, model_path, device, torch_dtype, revision="main"):
+        return load_compress_model(
+            model_path,
+            device,
+            torch_dtype,
+            use_fast=self.use_fast_tokenizer,
+            revision=revision,
+        )
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("ibm-labrador")
 # Note: the registration order matters.
 # The one registered earlier has a higher matching priority.
 register_model_adapter(PeftModelAdapter)
