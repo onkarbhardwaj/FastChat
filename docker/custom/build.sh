15c5f3a32bae64170a551cd7b96fc791872d7486
Veronique Demers <vdemers@ibm.com>
2024-08-06 20:10:01 +0000

    working

--- docker/custom/build.sh
+++ docker/custom/build.sh
@@ -1,1 +1,3 @@
-docker build -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/vllm:cuda12.4 .
+# docker build -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/vllm:cuda12.4 .
+
+docker build -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/vllm:cuda12.5 .
