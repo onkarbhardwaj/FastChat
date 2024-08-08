df4f0c4a7bc3c407a69798b3a6991831d89269ef
Veronique Demers <vdemers@ibm.com>
2024-08-08 17:58:31 +0000

    Docker image updated with nvidia cuda devel to have nvcc

--- docker/custom/build.sh
+++ docker/custom/build.sh
@@ -1,3 +1,1 @@
-# docker build -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/vllm:cuda12.4 .
-
 docker build -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/vllm:cuda12.5 .
