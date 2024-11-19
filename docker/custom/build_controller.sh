f1b68a18b7f7f8002ea467529f2009120398eb07
Veronique Demers <vdemers@ibm.com>
2024-11-19 13:23:08 +0000

    update

--- docker/custom/build_controller.sh
+++ docker/custom/build_controller.sh
@@ -3,4 +3,4 @@
 # Aside from the regular endpoints of the fastchat model-controller, this image also has
 # /v1/completions and /v1/chat/completitions, which uses the openai client to query the vllm model
 
-docker build --no-cache -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/fsctrl:cuda12.5_openai .
+docker build --no-cache -f Dockerfile --build-arg GH_TOKEN=$GH_TOKEN --build-arg GH_TOKEN_PUB=$GH_TOKEN_PUB -t us.icr.io/chatterina/fsctrl:cuda12.5_openai_dev .
